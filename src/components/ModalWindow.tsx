337dddca67b08c0666e8a6869be5cfa4a9ff37d2
monkeycs60 <clement.serizay@gmail.com>
2023-06-27 22:37:30 +0200

    fix: replace x string by svg

--- src/components/ModalWindow.tsx
+++ src/components/ModalWindow.tsx
@@ -21,7 +21,12 @@ export const ModalWindow = ({ isOpen, onClose, children }: ModalProps) => {
 						onClick={() => {
 							onClose();
 						}}>
-							X
+						<svg
+							className='h-4 w-4 fill-current'
+							xmlns='http://www.w3.org/2000/svg'
+							viewBox='0 0 24 24'>
+							<path d='M18.3 5.71a1 1 0 0 0-1.42 0L12 10.59l-4.88-4.88a1 1 0 1 0-1.42 1.42L10.59 12l-4.88 4.88a1 1 0 0 0 0 1.42 1 1 0 0 0 1.42 0L12 13.41l4.88 4.88a1 1 0 0 0 1.42 0 1 1 0 0 0 0-1.42L13.41 12l4.88-4.88a1 1 0 0 0 0-1.41z' />
+						</svg>
 					</button>
 					{children}
 				</div>
